e367d7d34138372d4a6c0dc526d9665b677fa5c3
athazahrane <elangathazahran@smkwikrama.sch.id>
2024-02-01 15:11:34 +0700

    ada perubahan dikit di breakpoint

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,10 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>CineBar | Login</title>
+    <title>Infak Wikrama | Login</title>
+
+    {{-- icon web --}}
+    <link rel="icon website" type="icon" href="{{ asset('image/icon-web.jpg') }}">
 
     <!-- my css -->
     <link rel="stylesheet" href="{{asset('css/login.css')}}" />
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,7 +3,10 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>CineBar | Register</title>
+    <title>Register Wikrama | Register</title>
+
+    {{-- icon web --}}
+    <link rel="icon website" type="icon" href="{{ asset('image/icon-web.jpg') }}">
 
     <!-- my css -->
     <link rel="stylesheet" href="{{asset('css/register.css')}}" />
