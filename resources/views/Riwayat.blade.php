bc40fdfa479ba39083a00f4155d5d0eeb31dce4e
wirdaalmahera <Wirdaalmahera@smkwikrama.sch.id>
2024-02-11 20:50:11 +0700

    new

--- resources/views/Riwayat.blade.php
+++ resources/views/Riwayat.blade.php
@@ -4,33 +4,97 @@
 
 @section('content')
 
-<div class="card mt-3">
-    <h5 class="card-header text-light" style="background-color: #3421C0" >Riwayat Pembayaran</h5>
-    <div class="card-body">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Nama Siswa</th>
-                    <th>Nominal Infaq</th>
-                    <th>tanggal Pembayaran</th>
-                    <th>Bulan</th>
-                    <th>action</th>
-                </tr>
-            </thead>
-            <tbody>
-                    
-                <tr>
-                    <td>1</td>
-                    <td>Aliyah</td>
-                    <td>20.000</td>
-                    <td>19/02/02</td>
-                    <td>September</td>
-                    <td>Terima</td>
-                </tr>
+<div>
+    <main>
+        <div class="head-title">
+            <div class="left">
+                <h1>Riwayat Pembayaran</h1>
+                <ul class="breadcrumb">
+                    <li>
+                        <a href="#">Riwayat</a>
+                    </li>
+                    <li><i class='bx bx-chevron-right' ></i></li>
+                    <li>
+                        <a class="active" href="/">Home</a>
+                    </li>
+                </ul>
+            </div>
+            <a href="#" class="btn-download">
+                <i class='bx bxs-cloud-download' ></i>
+                <span class="text">Download PDF</span>
+            </a>
+        </div>
 
-            </tbody>
-        </table>
-    </div>
+        {{-- <ul class="box-info">
+            <li>
+                <i class='bx bxs-calendar-check' ></i>
+                <span class="text">
+                    <h3>1020</h3>
+                    <p>New Order</p>
+                </span>
+            </li>
+            <li>
+                <i class='bx bxs-group' ></i>
+                <span class="text">
+                    <h3>2834</h3>
+                    <p>Visitors</p>
+                </span>
+            </li>
+            <li>
+                <i class='bx bxs-dollar-circle' ></i>
+                <span class="text">
+                    <h3>$2543</h3>
+                    <p>Total Sales</p>
+                </span>
+            </li>
+        </ul> --}}
+
+
+        <div class="table-data">
+            <div class="order">
+                <div class="head">
+                    <h3>Riwayat Pembayaran</h3>
+                    <i class='bx bx-search' ></i>
+                    <i class='bx bx-filter' ></i>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>User</th>
+                            <th>Date Order</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+            <div class="todo">
+                <div class="head">
+                    <h3>Todos</h3>
+                    <i class='bx bx-plus' ></i>
+                    <i class='bx bx-filter' ></i>
+                </div>
+                {{-- <ul class="todo-list">
+                    <li class="completed">
+                        <p>Todo List</p>
+                        <i class='bx bx-dots-vertical-rounded' ></i>
+                    </li>
+                    <li class="completed">
+                        <p>Todo List</p>
+                        <i class='bx bx-dots-vertical-rounded' ></i>
+                    </li>
+                    <li class="not-completed">
+                        <p>Todo List</p>
+                        <i class='bx bx-dots-vertical-rounded' ></i>
+                    </li>
+                    <li class="completed">
+                        <p>Todo List</p>
+                        <i class='bx bx-dots-vertical-rounded' ></i>
+                    </li>
+                    <li class="not-completed">
+                        <p>Todo List</p>
+                        <i class='bx bx-dots-vertical-rounded' ></i>
+                    </li> --}}
+                </ul>
+            </div>
 </div>
 @endsection
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,112 +1,183 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<title>Infaq | @yield ('title') </title>
 	<meta charset="UTF-8">
-    <title>Infaq | @yield ('title') </title>
-    <link rel="stylesheet" href="{{ asset('css/layout.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Boxicons -->
+	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
-</head>
+	<!-- My CSS -->
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="{{ asset('css/layout.css') }}">
+	<script src="{{ asset('js/layout.js') }}"></script>
 
+	<title>Infaq Wikrama</title>
+</head>
 <body>
 
-<div class="wrapper">
 
-	<div class="top_navbar">
-		<div class="logo">
-			<a href="#" style="text-decoration: none">Infaq Wikrama</a>
-		</div>
-		<div class="top_menu">
-			<div class="home_link">
-				<a href="#" style="text-decoration: none">
-					<span class="icon"><i class="fas fa-home"></i></span>
-					<span>Home</span>
+	<!-- SIDEBAR -->
+	<section id="sidebar">
+		<a href="#" class="brand" style="margin-left: 20px">
+			<img src="{{ asset('image/icon-web.jpg') }}" width="30" alt="">
+			<span class="text" style="margin-left: 15px">Infaq Wikrama</span>
+		</a>
+		<ul class="side-menu top">
+			<li class="active">
+				<a href="/dashboard">
+					<i class='bx bxs-dashboard' ></i>
+					<span class="text">Dashboard</span>
+				</a>
+			</li>
+			<li>
+				<a href="/pembayaran">
+					<i class='bx bxs-credit-card' ></i>
+					<span class="text">Pembayaran</span>
+				</a>
+			</li>
+			<li>
+				<a href="/riwayat">
+					<i class='bx bxs-time-five' ></i>
+					<span class="text">Riwayat</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class='bx bxs-book' ></i>
+					<span class="text">Tagihan</span>
+				</a>
+			</li>
+		</ul>
+		<ul class="side-menu">
+			<li>
+				<a href="#" class="logout">
+					<i class='bx bxs-log-out-circle' ></i>
+					<span class="text">Logout</span>
+				</a>
+			</li>
+		</ul>
+	</section>
+	<!-- SIDEBAR -->
+
+
+
+	<!-- CONTENT -->
+	<section id="content">
+		<!-- NAVBAR -->
+		<nav>
+			<form action="#">
+				<div class="form-input">
+					<input type="search" placeholder="Search...">
+					<button type="submit" class="search-btn"><i class='bx bx-search' ></i></button>
+				</div>
+			</form>
+		</nav>
+		<!-- NAVBAR -->
+
+		<!-- MAIN -->
+		{{-- <main>
+			<div class="head-title">
+				<div class="left">
+					<h1>Dashboard</h1>
+					<ul class="breadcrumb">
+						<li>
+							<a href="#">Dashboard</a>
+						</li>
+						<li><i class='bx bx-chevron-right' ></i></li>
+						<li>
+							<a class="active" href="/">Home</a>
+						</li>
+					</ul>
+				</div>
+				<a href="#" class="btn-download">
+					<i class='bx bxs-cloud-download' ></i>
+					<span class="text">Download PDF</span>
 				</a>
 			</div>
-			<div class="right_info">
-				<div class="icon_wrap">
-					<div class="icon">
-						<i class="fas fa-bell"></i>
+
+			<ul class="box-info">
+				<li>
+					<i class='bx bxs-calendar-check' ></i>
+					<span class="text">
+						<h3>1020</h3>
+						<p>New Order</p>
+					</span>
+				</li>
+				<li>
+					<i class='bx bxs-group' ></i>
+					<span class="text">
+						<h3>2834</h3>
+						<p>Visitors</p>
+					</span>
+				</li>
+				<li>
+					<i class='bx bxs-dollar-circle' ></i>
+					<span class="text">
+						<h3>$2543</h3>
+						<p>Total Sales</p>
+					</span>
+				</li>
+			</ul>
+
+
+			<div class="table-data">
+				<div class="order">
+					<div class="head">
+						<h3>Recent Orders</h3>
+						<i class='bx bx-search' ></i>
+						<i class='bx bx-filter' ></i>
 					</div>
+					<table>
+						<thead>
+							<tr>
+								<th>User</th>
+								<th>Date Order</th>
+								<th>Status</th>
+							</tr>
+						</thead>
+					</table>
 				</div>
-				<div class="icon_wrap">
-					<div class="icon">
-						<i class="fas fa-cog"></i>
+				<div class="todo">
+					<div class="head">
+						<h3>Todos</h3>
+						<i class='bx bx-plus' ></i>
+						<i class='bx bx-filter' ></i>
 					</div>
-				</div>
+					{{-- <ul class="todo-list">
+						<li class="completed">
+							<p>Todo List</p>
+							<i class='bx bx-dots-vertical-rounded' ></i>
+						</li>
+						<li class="completed">
+							<p>Todo List</p>
+							<i class='bx bx-dots-vertical-rounded' ></i>
+						</li>
+						<li class="not-completed">
+							<p>Todo List</p>
+							<i class='bx bx-dots-vertical-rounded' ></i>
+						</li>
+						<li class="completed">
+							<p>Todo List</p>
+							<i class='bx bx-dots-vertical-rounded' ></i>
+						</li>
+						<li class="not-completed">
+							<p>Todo List</p>
+							<i class='bx bx-dots-vertical-rounded' ></i>
+						</li> --}}
+					</ul>
+				</div> 
+				@yield('content')
 			</div>
-		</div>
-	</div>
-
-	<div class="main_body">
-		
-		<div class="sidebar_menu">
-	        <div class="inner__sidebar_menu">
-	        	
-	        	<ul>
-		          <li>
-		            <a href="/dashboard"  @if (request()->route()->uri == 'dashboard') class='active' @endif>
-		              <span class="icon">
-		              	<i class="fas fa-border-all"></i></span>
-		              <span class="list">Dashboard</span>
-		            </a>
-		          </li>
-		          <li>
-		            <a href="/payment" @if (request()->route()->uri == 'payment') class='active' @endif>
-		              <span class="icon"><i class="fas fa-chart-pie"></i></span>
-		              <span class="list">Pembayaran</span>
-		            </a>
-		          </li>
-		          <li>
-		            <a href="/tagihan" @if (request()->route()->uri == 'tagihan') class='active' @endif>
-		              <span class="icon"><i class="fas fa-address-book"></i></span>
-		              <span class="list">Tagihan</span>
-		            </a>
-		          </li>
-		          <li>
-		            <a href="/riwayat" @if (request()->route()->uri == 'riwayat') class='active' @endif>
-		              <span class="icon"><i class="fas fa-address-card"></i></span>
-		              <span class="list">Riwayat</span>
-		            </a>
-		          </li>
-		          <li>
-		            <a href="/">
-		              <span class="icon"><i class="fab fa-blogger"></i></span>
-		              <span class="list">Logout</span>
-		            </a>
-		          </li>
-		        </ul>
-
-		        <div class="hamburger">
-			        <div class="inner_hamburger">
-			            <span class="arrow">
-			                <i class="fas fa-long-arrow-alt-left"></i>
-			                <i class="fas fa-long-arrow-alt-right"></i>
-			            </span>
-			        </div>
-			    </div>
-
-	        </div>
-	    </div>
-
-	    <div class="container">
-	    	<div class="item_wrap">
-                @yield('content')
-	    	</div>
-	    </div>
-	</div>
-</div>
+		</main>
+		<!-- MAIN -->
+	</section>
+	<!-- CONTENT -->
 	
 
+	<script src="script.js"></script>
 </body>
-<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script>
-    $(document).ready(function(){
-        $(".hamburger").click(function(){
-          $(".wrapper").toggleClass("active")
-        })
-    });
-</script>
 </html>
